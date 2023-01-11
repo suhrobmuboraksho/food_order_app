da1f016d09857030fbf36285bb19abf9cd6a1124
Sukhrob <suhrob.m89@gmail.com>
2023-01-11 11:15:44 -0800

    Added remove function for the Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,8 +11,13 @@ const Cart = ({ onClose }) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemAddHandler = (item) => {};
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
